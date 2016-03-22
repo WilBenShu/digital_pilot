c2ee379da3f086cc6860cb5e74bd5f996b6ad0d5
EgloffGuillaume <guillaume.egloff@gmail.com>
2016-03-22 12:47:47 +0100

    Backoffice Kevin
    Backoffice Kevin

--- src/DigitalPilot/BackOfficeBundle/Controller/BackOfficeController.php
+++ src/DigitalPilot/BackOfficeBundle/Controller/BackOfficeController.php
@@ -11,8 +11,32 @@ class BackOfficeController extends Controller
      * @Route("/backoffice", name="userBundle_backoffice")
      */
     public function indexAction()
+    {        
+        $currentUser = $this->getUser();
+        
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+        
+        return $this->render('BackOfficeBundle:Default:index.html.twig', array(
+            'user' => $users
+        ));
+    }
+    
+    /**
+     * @Route("/backoffice/user", name="view_user")
+     */
+    public function userAction()
     {
-        return $this->render('BackOfficeBundle:Default:index.html.twig');
+        $currentUser = $this->getUser();
+        
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+        
+        //var_dump($users);
+        
+        return $this->render('BackOfficeBundle:Default:view_user.html.twig', array(
+            'user' => $users
+        ));
     }
     
 }
