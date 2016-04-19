165bd721af5ca15093740c5966f25033db79e7f6
EgloffGuillaume <guillaume.egloff@gmail.com>
2016-04-19 14:49:39 +0200

    Bo kevin suite
    Bo kevin suite

--- src/DigitalPilot/BackOfficeBundle/Controller/BackOfficeController.php
+++ src/DigitalPilot/BackOfficeBundle/Controller/BackOfficeController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace DigitalPilot\BackOfficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -59,9 +58,76 @@ class BackOfficeController extends Controller
  
         //var_dump($allTraders);
         return $this->render('BackOfficeBundle:Default:info_user.html.twig', array(
-            'allTraders' => $allTraders,
+            'allTraders' => $allTraders
         ));
     }
+    
+        /**
+     * @Route("/profilBo", name="profil_user")
+     */
+    public function profilAction(Request $request)
+    {
+        $dm = $this->get('doctrine_mongodb')->getManager()->getRepository('CmsBundle:Trader');
+
+        $currentUser = $this->getUser();
+        $currentTrader = $dm->findOneByEmail($currentUser->getEmail());
+
+        // IF CUSTOMER DON'T EXIST
+        // ADD NEW CUSTOMER WITH EMAIL ONLY IF NOT EXIST
+        if($currentTrader == NULL) {
+            $dm2 = $this->get('doctrine_mongodb')->getManager();
+
+            $trader = new Trader();
+            $trader->setFirstName('');
+            $trader->setLastName('');
+            $trader->setEmail($currentUser->getEmail());
+            $trader->setPhone('');
+            $trader->setPortable('');
+            $trader->setAddress('');
+            $trader->setCompany('');
+            $trader->setCompanySize('');
+            $trader->setCompanyAddress('');
+
+            $currentTrader = $trader;
+
+            // Add le customer dans la table customer
+            $dm2->persist($trader);
+            $dm2->flush();
+        }
+
+
+        $trader = new Trader();
+        $form = $this->createForm(new ProfilType(), $trader);
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+            if ($form->isValid()) {
+                //Actions à effecter après validation du form
+                $this->get('session')->getFlashBag()->add('notice', "Votre profil a bien été modifié ! ");
+
+                // Update le customer dans la table customer
+                $currentTrader = $dm->updateOneByMail($currentUser->getEmail(),
+                    $trader->getFirstName(),
+                    $trader->getLastName(),
+                    $trader->getPhone(),
+                    $trader->getPortable(),
+                    $trader->getAddress(),
+                    $trader->getCompany(),
+                    $trader->getCompanySize(),
+                    $trader->getCompanyAddress());
+            }
+        }
+        
+        
+
+        return $this->render('BackOfficeBundle:Default:profile_user.html.twig', array(
+            'user' => $currentUser,
+            'customer' => $currentTrader,
+            'form' => $form->createView(),
+        ));
+    }
+    
+
     
 }
     
